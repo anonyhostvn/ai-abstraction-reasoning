d2f23c9227bf5538ade7c39273a5fae544ac0ab2
Long Nguyễn <longnh07.aigrouplab@gmail.com>
2020-05-31 17:23:10 +0700

    add instruction

--- testing.module.py
+++ testing.module.py
@@ -1,4 +1,15 @@
 from read_data import InputMachine
 
-st = InputMachine.read_test_input("training")
-print(st[0].get('test'))
+# đọc tất cả các file trong folder training
+train_data = InputMachine.read_test_input("training")
+# Duyệt hết tất cả dữ liệu
+for data in train_data:
+    print(data.get("train"))
+    print(data.get("test"))
+    print(" ======== ")
+
+# đọc tất cả các file trong folder test
+test_data = InputMachine.read_test_input("test")
+
+# đọc tất cả các file trong folder evaluation
+evaluation_data = InputMachine.read_test_input("evaluation")
